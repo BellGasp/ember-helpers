1d0afc229f9dc5390149dc4dc77a254b11e863f0
Jean-Philippe Roy <jeanphilippe.roy@bell.ca>
2019-01-25 11:26:25 -0500

    Bugfix 🐛
    - Recomputes when the property of the object changes;

--- addon/helpers/translate.js
+++ addon/helpers/translate.js
@@ -1,28 +1,36 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 import { isEmpty } from '@ember/utils';
 import { classify } from '@ember/string';
-
-/**
-  @function translate
-  @param {EmberObject} object
-  @param {String} [locale = 'en']
-  @param {String} [attr = 'name']
-*/
-export function translate([obj, locale = 'en', attr = 'name']) {
-  if (isEmpty(obj)) {
-    return '';
-  }
-
-  // TODO: Find a way to give the opportunity to override this language hash.
-  // Might wanna give the opportunity to the consumer to provide it in their
-  // `environment.js` file.
-  const languages = { en: 'english', fr: 'french' };
-
-  return obj.get(`${languages[locale]}${classify(attr)}`);
-}
 
 /**
   @class Translate
   @public
 */
-export default helper(translate);
+export default Helper.extend({
+
+  /**
+    @function compute
+    @param {EmberObject} obj
+    @param {String} [locale = 'en']
+    @param {String} [attr = 'name']
+  */
+  compute([obj, locale = 'en', attr = 'name']) {
+    if (isEmpty(obj)) {
+      return '';
+    }
+
+    // TODO: Find a way to give the opportunity to override this language hash.
+    // Might wanna give the opportunity to the consumer to provide it in their
+    // `environment.js` file.
+    const languages = { en: 'english', fr: 'french' };
+
+    const key = `${languages[locale]}${classify(attr)}`;
+
+    this.set('obj', obj);
+
+    this.addObserver(`obj.${key}`, this, () => this.recompute());
+
+    return obj.get(key);
+  },
+
+});
